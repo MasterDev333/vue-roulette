8086b5bf49ef2d4f3e088b1345672dfe31c2c1e8
MasterDev333 <alexander_n1126@outlook.com>
2021-01-11 13:51:40 -0500

    audio issue

--- src/store/components/playAudio.js
+++ src/store/components/playAudio.js
@@ -1,23 +1,24 @@
 export const PLAY_AUDIO = (state) => {
-  console.log('this.audio_enabled')
+  console.log("this.audio_enabled");
   state.audio_disabled = false;
   state.audio_enabled = true;
 
   state.audio_bg.play();
-  state.kickSound1.play();
-  state.kickSound2.play();
-  state.kickSound3.play();
 
   state.kickSound3.volume = 1.0;
   state.kickSound2.volume = 1.0;
   state.kickSound1.volume = 1.0;
-  
-  state.audio_bg.addEventListener('ended', function() {
-    state.audio_bg.currentTime = 0;
-    state.audio_bg.play();
-  }, false);
-}
+
+  state.audio_bg.addEventListener(
+    "ended",
+    function() {
+      state.audio_bg.currentTime = 0;
+      state.audio_bg.play();
+    },
+    false
+  );
+};
 
 export const playAudio = (context) => {
-  context.commit('PLAY_AUDIO')
-}
+  context.commit("PLAY_AUDIO");
+};
